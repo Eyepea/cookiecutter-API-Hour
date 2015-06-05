4f9f4a22d2d792821df55a231878ab8223369c0b
Ludovic Gasc (GMLudo) <git@gmludo.eu>
2015-06-05 10:35:38 +0200

    Adapt make_servers method to the latest api_hour version for more flexibility

--- {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/__init__.py
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/__init__.py
@@ -30,12 +30,18 @@ class Container(api_hour.Container):
                                               '/{{cookiecutter.endpoint_name}}',
                                               endpoints.{{cookiecutter.endpoint_name}}.{{cookiecutter.endpoint_name}})
 
-    def make_servers(self):
-        # This method is used by api_hour command line to bind each server on each socket
-        return [self.servers['http'].make_handler(logger=self.worker.log,
-                                                  keep_alive=self.worker.cfg.keepalive,
-                                                  access_log=self.worker.log.access_log,
-                                                  access_log_format=self.worker.cfg.access_log_format)]
+    @asyncio.coroutine
+    def make_servers(self, sockets):
+        # This coroutine is used by api_hour command line to have the list of handlers
+        handlers = {}
+        handler = self.servers['http'].make_handler(logger=self.worker.log,
+                                                    keep_alive=self.worker.cfg.keepalive,
+                                                    access_log=self.worker.log.access_log,
+                                                    access_log_format=self.worker.cfg.access_log_format)
+        for sock in sockets:
+            srv = yield from self.loop.create_server(handler, sock=sock.sock)
+            handlers[srv] = handler
+        return handlers
 
     @asyncio.coroutine
     def start(self):
