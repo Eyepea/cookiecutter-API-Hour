f8763d9524dad116f271ccab5583bb329ddfe665
Ludovic Gasc (GMLudo) <gmludo@gmail.com>
2017-06-18 10:07:17 +0200

    Add systemd config files socket+service

--- {{cookiecutter.app_name}}/etc/{{cookiecutter.app_name}}/api_hour/gunicorn_conf.py
+++ {{cookiecutter.app_name}}/etc/{{cookiecutter.app_name}}/api_hour/gunicorn_conf.py
@@ -2,12 +2,12 @@ import multiprocessing
 import os
 
 workers = multiprocessing.cpu_count() * 2
-workers = 1 # dev mode
+workers = 1  # dev mode
 
 if os.environ.get('TRAVIS') == 'true':
     workers = 2
 
-bind = "0.0.0.0:8000"
+bind = ('0.0.0.0:8000', )
 keepalive = 15
 pidfile = '/run/lock/{{cookiecutter.app_name}}.pid'
 backlog = 10240000
